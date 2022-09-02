c71843a3ba7a171dc7f658d24a57a01e995d9c2c
universe100000 <112726829+universe100000@users.noreply.github.com>
2022-09-03 01:11:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Meal')
 streamlit.text('Kale, Spinach, Rocket Smoothie')
 streamlit.text('Half-Boiled Free-Range Egg')
