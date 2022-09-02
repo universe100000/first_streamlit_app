74ad083263cb5d5efb601e4cbffd5fd7b8ab4bee
universe100000 <112726829+universe100000@users.noreply.github.com>
2022-09-03 01:18:42 +0530

    Update streamlit_app.py
    added new header for fruits with emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Meal')
-streamlit.text('Kale, Spinach, Rocket Smoothie')
-streamlit.text('Half-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Meal')
+streamlit.text('🥗 Kale, Spinach, Rocket Smoothie')
+streamlit.text('🐔Half-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+  streamlit.text('Apple')
+  streamlit.text('Mango')
+  streamlit.text('Banana')
+  streamlit.text('Pineapple')
+  streamlit.text('Grapes')
+  
