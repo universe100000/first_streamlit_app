f5fa48ad7511f035d1633b8ab7cdab5aa27816fb
universe100000 <112726829+universe100000@users.noreply.github.com>
2022-09-03 22:44:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,5 @@ streamlit.dataframe(my_fruit_list)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
