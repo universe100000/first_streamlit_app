f6c7115ca1a6ecd3837dd616e1423fa535df44c7
universe100000 <112726829+universe100000@users.noreply.github.com>
2022-09-09 01:09:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,17 @@ import streamlit
 import pandas
 import snowflake.connector
 
+
+import streamlit
+
+import pandas
+
+import requests
+
+import snowflake.connector
+
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Meal')
